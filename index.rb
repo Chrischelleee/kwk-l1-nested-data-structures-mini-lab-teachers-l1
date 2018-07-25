0c7e51fa3177585cc2c0428880781afbdbd4ff6a
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-07-25 18:04:23 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -6,28 +6,34 @@ ingredients = [
     ["mayo", "mustard", "tabasco"]
 ]
 
-def sandwich_request(ingredients, order) 
-  # your code here!
+def sandwich_request(ingredients, order)
+  order.each do |want| #making the order iterate
+    found = ingredients.find do |arr| #if the other is found make a new array = found
+      arr.find {|have| want == have} # inside the array if we what we need was found then we want it inside the array and then want will be equals to what we have in the ingredients 
+    end
+    return false if !found #making the if statement true means it is false to meet the conditions set for it
+  end
+  true
 end
 
 # example city_info hash
 cities = {
-  :new_york => { 
+  :new_york => {
     :mayor => "Bill DeBlasio",
     :population => 8337000,
     :website => "http://www.nyc.gov"
   },
-  :los_angeles => { 
+  :los_angeles => {
     :mayor => "Eric Garcetti",
     :population => 3884307,
     :website => "http://www.lacity.org"
   },
-  :miami => { 
+  :miami => {
     :mayor => "Tomás Regalado",
     :population => 419777,
     :website => "http://www.miamigov.com"
   },
-  :chicago => { 
+  :chicago => {
     :mayor => "Rahm Emanuel",
     :population => 2695598,
     :website => "http://www.cityofchicago.org/"
@@ -35,5 +41,5 @@ cities = {
 }
 
 def city_populations(cities)
-  # your code here!
+  cities.sum {|city| city[:population]}
 end
